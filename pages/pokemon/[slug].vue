a15d001dff37a1331fcd040cd1457541b10f2877
Tr4ckTV <theo.a.gregoire@gmail.com>
2023-12-18 17:10:34 +0100

    maj

--- pages/pokemon/[slug].vue
+++ pages/pokemon/[slug].vue
@@ -1,5 +1,5 @@
 <script setup>
-const query = gql`
+const queryPokemon = gql`
   query Pokemon($slug: String!) {
     pokemon(where: { slug: $slug }) {
       id
@@ -7,39 +7,106 @@ const query = gql`
       slug
       description
       createdAt
-      publishedAt
-      updatedAt
-      stage
       image {
-        url(
-          transformation: {
-            image: { resize: { fit: crop, height: 1024, width: 1024 } }
-            document: { output: { format: webp } }
-          }
-        )
+        url
+      }
+      types_pokemons {
+        nom
+        logo {
+          url
+        }
       }
     }
   }
 `;
 
-const pokemon = ref();
+const pokemon = ref(null);
+const types = ref([]);
 
-const route = useRoute();
-const { data } = await useAsyncQuery(query, {
-  slug: route.params.slug,
-});
+const fetchPokemonData = async () => {
+  try {
+    const route = useRoute();
+    console.log("Slug:", route.params.slug);
 
-console.log(data.value);
-pokemon.value = data.value.pokemon;
+    const { data, errors } = await useAsyncQuery(queryPokemon, {
+      slug: route.params.slug,
+    });
 
+    console.log("Data:", data);
+    console.log("Errors:", errors); // Ajout de cette ligne
+
+    if (data && data.pokemon) {
+      pokemon.value = data.pokemon;
+      types.value = data.pokemon.types_pokemons;
+    } else {
+      console.error("Pokemon not found.");
+    }
+  } catch (error) {
+    console.error("Error fetching Pokemon data:", error);
+  }
+};
+
+// Appel à la fonction au montage du composant
+fetchPokemonData();
+
+const getTypeNames = (types) => {
+  return types.map((type) => type.nom).join(", ");
+};
 </script>
 
 <template>
-  <div v-if="pokemon" class="max-w-lg">
-    <NuxtImg :src="pokemon.image.url" :alt="pokemon.nom" />
-    <h2 class="text-3xl text-center">{{ pokemon.nom }}</h2>
+  <div v-if="pokemon" class="pokemon-details-container">
+    <div class="pokemon-header">
+      <h2 class="pokemon-name">{{ pokemon.nom }}</h2>
+    </div>
+    <div class="pokemon-content">
+      <div class="pokemon-image">
+        <NuxtImg :src="pokemon.image.url" :alt="pokemon.nom" />
+        <p class="pokemon-type">Type: {{ getTypeNames(types) }}</p>
+      </div>
+      <div class="pokemon-info">
+        <p class="pokemon-description">{{ pokemon.description }}</p>
+      </div>
+    </div>
   </div>
   <div v-else>
     <li>Loading...</li>
   </div>
 </template>
+
+<style scoped>
+.pokemon-details-container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.pokemon-header {
+  background-color: #fff;
+  padding: 10px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+
+.pokemon-name {
+  font-size: 2rem;
+  margin: 0;
+}
+
+.pokemon-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.pokemon-image {
+  width: 40%;
+}
+
+.pokemon-info {
+  width: 55%;
+}
+
+.pokemon-type {
+  font-weight: bold;
+}
+</style>
